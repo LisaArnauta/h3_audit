1dc1d2739a6af20aeb5ac53df7e4a84533049ea0
LisaArnauta <lizaarnauta@gmail.com>
2020-10-19 11:12:19 +0300

    page pattern

--- panel/urls.py
+++ panel/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('my_audits', views.my_audits, name='my_audits')
+    path('my_audits', views.my_audits, name='my_audits'),
+    path('registration', views.registration, name='registration')
 ]
--- panel/views.py
+++ panel/views.py
@@ -1,21 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.models import User
-
-from .models import Audit
-
+from panel.forms import RegistrationForm
+from django.shortcuts import redirect,render
 
 def index(request):
-    """
-    Main panel page
-    :param request:
-    :return:
-    """
-    return HttpResponse("Hello, audit.")
+    print()
 
+def registration(request):
+    context = {}
+    if request.POST:
+        form = RegistrationForm(request.POST)
+        context['form'] = form
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+        else:
+            context['form'] = form
+            return render(request, 'pattern.html', context)
 
-def my_audits(request):
-    audits = Audit.objects.filter(owner_id=2)
-    audits = "\n".join([f"{a.id} - {a.main_url}"
-                       for a in audits])
-    return HttpResponse(f"Your audits: \n{audits}")
+#if there's GET request  or smth else
+
+    else:
+        if request.user.is_authenticated:
+            return redirect("index")
+        else:
+            form = RegistrationForm()
+            context['form'] = form
+            return render(request,'pattern.html',context)
+
