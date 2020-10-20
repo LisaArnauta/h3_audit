77020cfcc904d1d88f85fa10308352eb2774bbc1
LisaArnauta <lizaarnauta@gmail.com>
2020-10-20 09:51:52 +0300

    registration views

--- panel/forms.py
+++ panel/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class RegistrationForm(UserCreationForm):
     email = forms.CharField(label="email", widget=forms.EmailInput)
+
     class Meta():
         model = User
         fields = ('username', 'email', 'password1', 'password2')
