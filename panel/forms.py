bdb1bbc7f3a8f656ceac7eebf47c9855c24cafe4
LisaArnauta <lizaarnauta@gmail.com>
2020-10-20 10:06:33 +0300

    registration forms

--- panel/forms.py
+++ panel/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegistrationForm(UserCreationForm):
-    email = forms.CharField(label="email", widget=forms.EmailInput)
+    email = forms.CharField(label="emails", widget=forms.EmailInput)
 
     class Meta():
         model = User
